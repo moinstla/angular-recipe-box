0d4dd1a8a210879aae033b07e5b505b928eecf02
Kai de Rochemont and Monique St. Laurent <monique@>
2017-06-06 16:39:27 -0700

    finish this thang

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,8 +2,76 @@ import { Component } from '@angular/core';
 import { Recipe } from './recipe';
 
 const RECIPES: Recipe[] = [
-  { title: 'Airplane Food', description: 'no other choice', imageURL: 'https://cdn1.vox-cdn.com/uploads/chorus_asset/file/3700298/airplane-food.0.jpg', ingredients: ['plastic', 'something resembling bread', 'plane gravy', 'mystery meat'], directions: ['microwave', 'remove top layer of plastic', 'charge $10'] },
-  { title: 'Cat Food', description: 'no other choice', ingredients: ['plastic', 'something resembling bread', 'plane gravy', 'mystery meat'], directions: ['microwave', 'remove top layer of plastic', 'charge $10'] }
+  { title: 'Airplane Food',
+    description: 'no other choice',
+    imageURL: 'https://cdn1.vox-cdn.com/uploads/chorus_asset/file/3700298/airplane-food.0.jpg',
+    ingredients: ['plastic', 'something resembling bread', 'plane gravy', 'mystery meat'],
+    directions: ['microwave', 'remove top layer of plastic', 'charge $10'],
+    tried: true;
+  },
+  { title: 'Cat Food',
+    description: 'better than dog foood',
+    imageURL: 'http://www.thehealthycookingcoach.com/wp-content/uploads/2015/08/cat-and-food.jpg',
+    ingredients: ['plastic', 'something resembling bread', 'plane gravy', 'mystery meat'],
+    directions: ['microwave', 'remove top layer of plastic', 'charge $10'],
+    tried: false;
+  },
+  { title: 'Pazta',
+    description: 'like small, edible snakes',
+    imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Pasta_2006_6.jpg/406px-Pasta_2006_6.jpg',
+    ingredients: ['plastic', 'something resembling bread', 'plane gravy', 'mystery meat'],
+    directions: ['microwave', 'remove top layer of plastic', 'charge $10'],
+    tried: true;
+  },
+  { title: 'Catplane Food',
+    description: 'yay',
+    imageURL: '',
+    ingredients: ['plastic', 'something resembling bread', 'plane gravy', 'mystery meat'],
+    directions: ['microwave', 'remove top layer of plastic', 'charge $10'],
+    tried: true;
+  },
+  { title: 'La-zag-na',
+    description: 'layers, yay',
+    imageURL: 'https://s-media-cache-ak0.pinimg.com/originals/d9/73/6b/d9736bba80d5ab7a1513e29ada878549.jpg',
+    ingredients: ['plastic', 'something resembling bread', 'plane gravy', 'mystery meat'],
+    directions: ['microwave', 'remove top layer of plastic', 'charge $10'],
+    tried: true;
+  },
+  { title: 'Meat Stuff',
+    description: 'it used to be alive',
+    imageURL: '',
+    ingredients: ['plastic', 'something resembling bread', 'plane gravy', 'mystery meat'],
+    directions: ['microwave', 'remove top layer of plastic', 'charge $10'],
+    tried: true;
+  },
+  { title: 'Lunchables',
+    description: 'pizza, of course',
+    imageURL: 'https://cdn1.vox-cdn.com/uploads/chorus_asset/file/3700298/airplane-food.0.jpg',
+    ingredients: ['plastic', 'something resembling bread', 'plane gravy', 'mystery meat'],
+    directions: ['microwave', 'remove top layer of plastic', 'charge $10'],
+    tried: false;
+  },
+  { title: 'Super Soup',
+    description: 'Water featuring: other stuff',
+    imageURL: '',
+    ingredients: ['plastic', 'something resembling bread', 'plane gravy', 'mystery meat'],
+    directions: ['microwave', 'remove top layer of plastic', 'charge $10'],
+    tried: false;
+  },
+  { title: 'Fried Dragon',
+    description: 'ironic?',
+    imageURL: 'http://www.thinkgeek.com/images/products/zoom/1144_canned_dragon_meat.jpg',
+    ingredients: ['plastic', 'something resembling bread', 'plane gravy', 'mystery meat'],
+    directions: ['microwave', 'remove top layer of plastic', 'charge $10'],
+    tried: false;
+  },
+  { title: 'Serious Food',
+    description: 'seriously delicious',
+    imageURL: '',
+    ingredients: ['plastic', 'something resembling bread', 'plane gravy', 'mystery meat'],
+    directions: ['microwave', 'remove top layer of plastic', 'charge $10'],
+    tried: true;
+  },
 ];
 
 @Component({
@@ -15,55 +83,99 @@ const RECIPES: Recipe[] = [
     </div>
   </div>
   <div class="container">
-    <div class="recipe" *ngFor="let recipe of recipes">
-      <div class="row">
-      <h1 class="recipe-title" (click)="toggleShow(recipe)">{{recipe.title}}</h1>
-      </div>
-      <div class="row recipe-details" *ngIf="recipe === this.selectedRecipe">
-        <div class="col-md-4">
-          <p>{{recipe.description}}</p>
-          <img *ngIf="recipe.imageURL" src={{recipe.imageURL}}>
-          <img *ngIf="!recipe.imageURL" src="http://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/quizzes/food_safety_quiz/650x350_food_safety_quiz.jpg">
-          <button (click)="editRecipe(recipe)">Edit Recipe</button>
-        </div>
-        <div class="col-md-4">
-          <h3>ingredients:</h3>
-          <ul>
-            <li *ngFor="let ingredient of recipe.ingredients">
-            {{ingredient}}
-            </li>
-          </ul>
-        </div>
-        <div class="col-md-4">
-        <h3>directions:</h3>
+  <div class="col-md-6" id="recipe-list">
+    <ul>
+      <li *ngFor="let recipe of recipes" (click)="selectRecipe(recipe)" [class.checked]="recipe.tried === true" [class.selected]="recipe === selectedRecipe" >{{recipe.title}}</li>
+    </ul>
+  </div>
+  <div class="col-md-6" id="selected-recipe">
+    <div *ngIf="selectedRecipe">
+    <div class="checkbox">
+    <label><input type="checkbox" [(ngModel)]="selectedRecipe.tried" [value]="true">Tried Recipe</label>
+    </div>
+      <h1>{{selectedRecipe.title}}</h1>
+      <h3>{{selectedRecipe.description}}</h3>
+      <img *ngIf="selectedRecipe.imageURL" [src]="selectedRecipe.imageURL">
+      <img *ngIf="!selectedRecipe.imageURL" src="http://orewa.org/wp-content/uploads/2013/12/healthy-food-pictures-tumblr.jpg">
+      <div id="recipe-details">
+        <h5>Ingredients:</h5>
         <ul>
-          <li *ngFor="let direction of recipe.directions">
-          {{direction}}
-          </li>
+          <li *ngFor="let ingredient of selectedRecipe.ingredients">{{ingredient}}</li>
         </ul>
-
+        <h5>Directions:</h5>
+        <ul>
+          <li *ngFor="let direction of selectedRecipe.directions">{{direction}}</li>
+        </ul>
+      </div>
+      <button class="btn btn-info"  *ngIf="!formDisplay" (click)='toggleEditForm()'>Edit Recipe</button>
+      <div *ngIf="formDisplay" id="edit-form">
+        <div class='form-group'>
+          <label>Title</label>
+          <input
+          class="form-control" [(ngModel)]="selectedRecipe.title">
         </div>
+        <div class='form-group'>
+          <label>Description</label>
+          <input
+          class="form-control"
+          [(ngModel)]="selectedRecipe.description">
+        </div>
+        <div class='form-group form-inline'>
+          <label>Add ingredient</label>
+          <input
+          class="form-control"
+          [(ngModel)]="ingredient">
+          <button class="btn btn-primary" (click)="addIngredient(selectedRecipe, ingredient)">
+          Add
+          </button>
+        </div>
+        <div class='form-group form-inline'>
+          <label>Add direction</label>
+          <input
+          class="form-control"
+          [(ngModel)]="direction">
+          <button class="btn btn-primary" (click)="addDirection(selectedRecipe, direction)">
+          Add
+          </button>
+        </div>
+        <button class="btn btn-success" (click)='toggleEditForm()'>Done</button>
       </div>
     </div>
   </div>
+  </div>
   `
 })
+
 
 export class AppComponent {
   title = "Recipe Box";
   recipes = RECIPES;
   selectedRecipe: Recipe;
+  formDisplay: boolean = false;
 
-editRecipe(recipe: Recipe) {
-  console.log(recipe.imageURL);
-}
+  editRecipe(recipe: Recipe) {
+    console.log(recipe.imageURL);
+  }
 
-  toggleShow(recipe: Recipe): void {
-    if (this.selectedRecipe === recipe) {
-      this.selectedRecipe = null;
+  selectRecipe(recipe: Recipe) {
+    this.selectedRecipe = recipe;
+  }
+
+  addIngredient(recipe: Recipe, ingredient: String) {
+    recipe.ingredients.push(ingredient);
+  }
+
+  addDirection(recipe: Recipe, direction: String) {
+    recipe.directions.push(direction);
+  }
+
+  toggleEditForm() {
+    if(this.formDisplay === true) {
+      this.formDisplay = false;
     } else {
-      this.selectedRecipe = recipe;
+      this.formDisplay = true;
     }
   }
+
 
 }
--- app/recipe.ts
+++ app/recipe.ts
@@ -1,3 +1,3 @@
 export class Recipe {
-  constructor(public title: string, public description: string, public imageURL: string, public ingredients: string[], public directions: string[]) { }
+  constructor(public title: string, public description: string, public imageURL: string, public ingredients: string[], public directions: string[], public tried: boolean) { }
 }
